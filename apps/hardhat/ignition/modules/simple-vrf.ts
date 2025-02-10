bf80240bb9e54fb9370adf733f90a9273f8c5545
Nicholas St. Germain <nick@cajun.pro>
2025-02-10 05:27:39 -0600

    add deployment

--- apps/hardhat/ignition/modules/simple-vrf.ts
+++ apps/hardhat/ignition/modules/simple-vrf.ts
@@ -3,7 +3,7 @@ import { Address } from "viem";
 
 const VRF_COORDINATOR: Address = "0x5C210eF41CD1a72de73bF76eC39637bB0d3d7BEE";
 const SUB_ID: bigint = 71049531047156375077720865804266575258889020338648943929284138857790579591324n;
-const KEY_HASH: Address = "0x354d2f95da55398f44b7cff77da56283d9c6c829a4bdf1bbcaf2ad6a4d081f61";
+const KEY_HASH: Address = "0xc799bd1e3bd4d1a41cd4968997a4e03dfd2a3c7c04b695881138580163f42887";
 
 const SimpleVRFModule = buildModule("SimpleVRFModule", (m) => {
   // address vrfCoordinator, uint64 subId, bytes32 _keyHash
